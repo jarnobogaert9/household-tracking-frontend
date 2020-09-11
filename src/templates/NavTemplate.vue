399adf432b86136d32b458c6a44a700d74842280
Jarno Bogaert <jarnobogaert9@gmail.com>
2020-09-11 15:44:08 +0200

    Feat: template with navbar

---
+++ src/templates/NavTemplate.vue
@@ -0,0 +1,52 @@
+<template>
+<div>
+    <b-navbar type="is-white">
+        <template slot="brand">
+            <b-navbar-item tag="router-link" :to="{ path: '/' }">
+                <img
+                    src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
+                    alt="Lightweight UI components for Vue.js based on Bulma"
+                >
+            </b-navbar-item>
+        </template>
+        <template slot="start">
+            <b-navbar-item href="#">
+                Home
+            </b-navbar-item>
+            <b-navbar-item href="#">
+                Types
+            </b-navbar-item>
+            <b-navbar-item href="#">
+                Users
+            </b-navbar-item>
+        </template>
+
+        <template slot="end">
+            <b-navbar-item tag="div">
+                <div class="buttons">
+                    <a class="button is-primary">
+                        <strong>Sign up</strong>
+                    </a>
+                    <a class="button is-light">
+                        Log in
+                    </a>
+                </div>
+            </b-navbar-item>
+        </template>
+    </b-navbar>
+    <!-- Slot to load other views -->
+    <div class="container">
+        <slot></slot>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
